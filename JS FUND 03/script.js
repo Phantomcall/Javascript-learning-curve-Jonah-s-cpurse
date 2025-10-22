94c88fe377f65dcc76c88b1ee0d81163a49c1415
Phantom-Call <pinzypatz@gmail.com>
2025-10-22 12:49:38 +0100

    Arrays and Array methods

--- JS FUND 03/script.js
+++ JS FUND 03/script.js
@@ -90,7 +90,6 @@ function fruitProcessor(apples, oranges) {
 }
 
 console.log(fruitProcessor(2, 3));
-*/
 
 // REVIEWING FUNCTIONS
 
@@ -113,3 +112,107 @@ const yearsUntilRetirement = function (birthYear, firstName) {
 
 console.log(yearsUntilRetirement(1991, `Patrick`));
 console.log(yearsUntilRetirement(1950, `Jonas`));
+
+// CHALLENGE #1 FUNCTIONS:
+const calcAverage = (score1, score2, score3) => {
+  const averageScores = (score1 + score2 + score3) / 3;
+  return averageScores;
+}
+
+let scoreDolphins = console.log(calcAverage(44, 23, 71));
+let scoreKoalas = console.log(calcAverage(65, 54, 49));
+
+function checkWinner(avgDolphins, avgKoalas) {
+
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win ${avgKoalas} vs. ${avgDolphins}`);
+  } else {
+    console.log(`No team wins`);
+  }
+}
+
+
+// TRST DATA 1
+checkWinner(scoreDolphins, scoreKoalas);
+
+// TEST DATA 2
+scoreDolphins = (calcAverage(85, 54, 41));
+scoreKoalas = (calcAverage(23, 34, 27));
+console.log(scoreDolphins, scoreKoalas);
+checkWinner(scoreDolphins, scoreKoalas);
+
+
+// ARRAYS
+const friends = [`Michael`, `Steven`, `Peter`];
+console.log(friends);
+
+const years = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = `James`;
+console.log(friends);
+
+const firstName = `Jonas`;
+const jonas = [firstName, `Schmedtmann`, 2037 - 1991, `Teacher`, friends];
+
+console.log(jonas);
+
+// ARRAY EXERCISE
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const years1 = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1]),
+];
+
+console.log(ages);
+
+
+// ARRAY METHODS:
+const friends = [`Michael`, `Steven`, `Peter`];
+
+// ADD ELEMENTS
+const newLength = friends.push(`James`);
+console.log(friends);
+console.log(newLength);
+
+friends.unshift(`Dana`);
+console.log(friends);
+
+// REMOVE ELEMENTS
+const popped = friends.pop();
+console.log(friends);
+console.log(popped);
+
+friends.shift();
+console.log(friends);
+
+console.log(friends.indexOf(`Steven`));
+
+friends.push(23);
+console.log(friends.includes(`Steven`));
+console.log(friends.includes(`23`));
+
+if (friends.includes(`Peter`)) {
+  console.log(`You have a friend called peter`);
+} else {
+  console.log(`You have no friedns, loser!!!`);
+}
+*/
