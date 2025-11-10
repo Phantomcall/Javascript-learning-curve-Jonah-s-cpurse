212f6d1c44f84eadb1d3fffe9785f4a3ff5f8280
Phantom-Call <pinzypatz@gmail.com>
2025-11-10 12:06:00 +0100

    i will continue the task later

--- dev skills and setup/script.js
+++ dev skills and setup/script.js
@@ -103,7 +103,6 @@ const calcTempAmplitudebug = function (t1, t2) {
 
 const amplitudebug = calcTempAmplitudebug([3, 5, 1], [9, 4, 5]);
 console.log(amplitudebug);
-*/
 
 // TASK TO PRINT PUT A STRING WITH VALUES FROM AN ARRAY:
 const data1 = [17, 21, 23];
@@ -122,3 +121,123 @@ const printForecast = function (arr) {
 
 // SINCE THE FUNCTION IS NOT HARD CODED ANY VALUE GIVEN IN THE INVOKE OF THE FUNCTION WILL WORK BASED ON THE LOOP
 printForecast(data1);
+*/
+
+// MY PERSONAL CHALLENGE:
+
+// DATA: EMPLOYESS
+const employees = [
+  {
+    name: "Alex Johnson",
+    role: "manager",
+    hoursWorked: 180,
+    hourlyRate: 35,
+    bonuses: [300, 150, 200],
+  },
+  {
+    name: "Jamie Lee",
+    role: "developer",
+    hoursWorked: 160,
+    hourlyRate: 28,
+    bonuses: [100, 250],
+  },
+  {
+    name: "Taylor Brooks",
+    role: "intern",
+    hoursWorked: 120,
+    hourlyRate: 15,
+    bonuses: [],
+  },
+  {
+    name: "Morgan Smith",
+    role: "developer",
+    hoursWorked: 175,
+    hourlyRate: 30,
+    bonuses: [400, 200, 100],
+  },
+  {
+    name: "Riley Carter",
+    role: "designer",
+    hoursWorked: 155,
+    hourlyRate: 25,
+    bonuses: [150],
+  },
+  {
+    name: "Jordan Reed",
+    role: "sales",
+    hoursWorked: 190,
+    hourlyRate: 20,
+    bonuses: [300, 300, 100],
+  },
+];
+
+// FIRST FUNCTION TO CALCULATE THE BASEPAY FOR EACh EMPLOYEE: (hoursworked * hourlyrate)
+//employees[0].basepay = 100;
+//console.log(employees);
+
+//const calcBasePay = function () {
+//  let basePay = (hoursWorked + hourlyRate);
+
+//  let base = 0;
+//  for
+//}
+
+//employees[0].BasePay = Number(
+//  employees[0].hoursWorked * employees[0].hourlyRate
+//);
+
+//  for (const i of employees) {
+//    const base = i.hoursWorked * i.hourlyRate;
+
+//    employees.BasePay = base;
+//  }
+
+//console.log(employees);
+
+const calcBasePay = function (arr) {
+  for (const x of employees) {
+    const total = x.hoursWorked * x.hourlyRate;
+    x.BasePay = total;
+  }
+};
+
+const base = calcBasePay(employees);
+console.log(employees);
+
+//for (let i = 0; i < emp.length; i++)
+//const hoursWorked = emp.map(ep => ep.hoursWorked);
+//const hourlyRate = emp.map(ep => ep.hourlyRate);
+
+//console.log("hours worked", emp);
+//console.log("", employees);
+//console.log("hour Rate", hourlyRate);
+// This is a function to calculate the total base pay for all employees
+//const hours = employees.map(employees => employees.hoursWorked);
+//const rates = employees.map(employees => employees.hourlyRate);
+
+//const calcBasePay = function (hour, rate) {
+//  let totalHours = 0;
+//  for (let i = 0; i < hour.length; i++) {
+//    totalHours += hour[i];
+//  }
+//  let totalRates = 0;
+//  for (let i = 0; i < rate.length; i++) {
+//    totalRates += rate[i];
+//  }
+
+//  console.log(totalHours, totalRates);
+//  return totalHours * totalRates;
+//};
+
+//const BasePay = calcBasePay(hours, rates);
+//console.log(BasePay);
+
+let ask = (question) => {
+  if (question == `yes`) {
+    alert(`You agreed`);
+  } else if (question == `no`) {
+    alert(`you canceled the execution`);
+  }
+}
+
+let answer = ask(`yes`);
