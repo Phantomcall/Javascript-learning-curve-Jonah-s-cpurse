417d29f0c0b299b606f19ec8a0e5eb71f6436b67
Phantom-Call <pinzypatz@gmail.com>
2026-03-05 23:37:12 +0100

    a closer look at functions

---
+++ CLOSER LOOK AT FUNCTIONS/scirpt.js
@@ -0,0 +1,68 @@
+"Use strict";
+
+// DEFAULT PARAMETERS:
+
+//const booking = [];
+
+//const createBooking = function (
+//  flightNum,
+//  numPassengers = 1,
+//  price = 199 * numPassengers,
+//) {
+//// ES5
+//  numPassengers = numPassengers || 1;
+//    price = price || 199;
+
+//  const funcBooking = {
+//    flightNum,
+//    numPassengers,
+//    price,
+//  };
+
+//  console.log(booking);
+//  booking.push(funcBooking);
+//};
+
+//createBooking("LH124");
+//createBooking("LH123", 2, 800);
+//createBooking("LH123", 2);
+//createBooking("LH123", 5);
+//createBooking("LH123", undefined, 1000);
+
+//const flight = `KD456`;
+//const patrick = {
+//  name: "Patrick Amune",
+//  passport: 23456789,
+//};
+
+//const checkIn = function (flightNum, passenger) {
+//  flightNum = "AB789";
+//  passenger.name = `Mr. ` + passenger.name;
+
+//  const passP = Number(prompt("Passport Number"));
+
+//  if (passenger.passport === passP) {
+//    alert(`Check in`);
+//  } else {
+//    alert(`Wrong passport`);
+//  }
+//};
+
+//checkIn(flight, patrick);
+//console.log(flight);
+//console.log(patrick);
+
+//const newPassPort = function (person) {
+//  person.passport = Math.trunc(Math.random() * 1000000);
+//  console.log(person.passport);
+//};
+
+//newPassPort(patrick);
+//checkIn(flight, patrick);
+
+//function counter() {
+//  let counter = 0;
+//  return function () {
+//    counter++;
+//  };
+//}
